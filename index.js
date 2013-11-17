7c4e35247d5b8114da3541d02df54ac425dc94b1
Dan Larsen <dan@techba.se>
2013-11-17 04:26:17 +0100

    Added unique and merge
    + ignoring logs

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 /**
+ * extend-array
+ * Extends the Array class with a couple of useful new methods
+ * They are all "hidden" with enumerable: hidden using defineProperty
+ */
+
+/**
  * Credits for this should go to Edgar Villegas Alvarado
  * He showed a fast intersect solution on Stack Overflow:
  * http://stackoverflow.com/questions/1885557/simplest-code-for-array-intersection-in-javascript
@@ -7,7 +13,6 @@
  * The additions make sure the arrays are sorted, but makes the code slower.
  * In return the original arrays doesn't get changed (sorted).
  */
-
 Array.prototype.intersect = function(arr) {
   // The last check here, is a check for a bug spotted, when running a large node.js App
   // Exact problem not located, but these checks ensures that there isn't an error thrown
@@ -28,40 +33,29 @@ Array.prototype.intersect = function(arr) {
 };
 Object.defineProperty(Array.prototype, 'intersect', { enumerable: false });
 
-
-
-// This is the original intersect code by Edgar Villegas Alvarado
-// Thank you! :)
-
-/* finds the intersection of
- * two arrays in a simple fashion.
- *
- * PARAMS
- *  a - first array, must already be sorted
- *  b - second array, must already be sorted
- *
- * NOTES
- *
- *  Should have O(n) operations, where n is
- *    n = MIN(a.length(), b.length())
+/**
+ * Credits for this should go to LiraNuna
+ * He showed this function on Stack Overflow:
+ * http://stackoverflow.com/questions/1584370/how-to-merge-two-arrays-in-javascript
  */
+Array.prototype.unique = function() {
+  if( this.toString() != '[object global]' ) {
+    var a = this;
+    for(var i=0; i<a.length; ++i) {
+      for(var j=i+1; j<a.length; ++j) {
+        if(a[i] === a[j]) {
+          a.splice(j--, 1);
+        }
+      }
+    }
+    return a;
+  }
+};
+Object.defineProperty(Array.prototype, 'unique', { enumerable: false });
 
-// function intersect_safe(a, b)
-// {
-//   var ai=0, bi=0;
-//   var result = new Array();
-
-//   while( ai < a.length && bi < b.length )
-//   {
-//      if      (a[ai] < b[bi] ){ ai++; }
-//      else if (a[ai] > b[bi] ){ bi++; }
-//      else /* they're equal */
-//      {
-//        result.push(a[ai]);
-//        ai++;
-//        bi++;
-//      }
-//   }
-
-//   return result;
-// }
+Array.prototype.merge = function() {
+  if( (arguments.length >= 1) && (this.toString() != '[object global]') ) {
+    return this.concat.apply(this, arguments).unique();
+  } else return this;
+};
+Object.defineProperty(Array.prototype, 'merge', { enumerable: false });
